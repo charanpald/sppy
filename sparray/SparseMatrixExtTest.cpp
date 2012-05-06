7dafae8e99f07f4a24e8acf9cbab131173a4db3f
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-06 21:38:37 +0200

    Test of iterator.

--- sparray/SparseMatrixExtTest.cpp
+++ sparray/SparseMatrixExtTest.cpp
@@ -8,11 +8,31 @@ using Eigen::SparseMatrix;
 
 int main()
 {
-  SparseMatrix<double, Eigen::RowMajor> m(2,2);
-  m.insert(0,0) = 3;
- m.insert(1,0) = 2.5;
-  m.insert(0,1) = -1;
+    SparseMatrix<double, Eigen::RowMajor> m(3,3);
+    m.insert(0,0) = 3;
+    m.insert(1,0) = 2.5;
+    m.insert(0,1) = -1;
+    
+    for (int k=0; k<m.outerSize(); ++k) {
+      for (SparseMatrix<double, Eigen::RowMajor>::InnerIterator it(m,k); it; ++it)
+      {
+        it.value();
+        it.row();   // row index
+        it.col();   // col index (here it is equal to k)
+        it.index(); // inner index, here it is equal to it.row()
 
-  //m.insert(1,1) = m(1,0) + m(0,1);
-  std::cout << m << std::endl;
+        std::cout << it.row() << ", " <<  it.col() << ", " <<  it.value() << std::endl; 
+        
+      }
+ }
+
+    std::cout << "Printing full matrix" << std::endl; 
+    for (int i=0;i<3;i++) { 
+        for (int j=0;j<3;j++) { 
+            std::cout << i << ", " <<  j << ", " <<  m.coeff(i, j) << std::endl;             
+            }        
+        }
+
+
+  //std::cout << m << std::endl;
 }

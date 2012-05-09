bf44247498f50043278794453ae43f47f96b5c8f
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-09 16:46:07 +0200

    Added slice and print methods.

--- sparray/include/DynamicSparseMatrixExt.h
+++ sparray/include/DynamicSparseMatrixExt.h
@@ -8,6 +8,8 @@
 using Eigen::DynamicSparseMatrix;
 
 template <class T>
+
+//Class is column major 
 class DynamicSparseMatrixExt:public DynamicSparseMatrix<T> {
   public:
 	DynamicSparseMatrixExt<T>(): 
@@ -20,6 +22,37 @@ class DynamicSparseMatrixExt:public DynamicSparseMatrix<T> {
     
     void insertVal(int row, int col, T val) { 
         this->coeffRef(row, col) = val;
+        }
+
+    void printValues() { 
+        for (int k=0; k<this->outerSize(); ++k) {
+          for (DynamicSparseMatrixExt<double>::InnerIterator it(*this,k); it; ++it) {
+            std::cout << "(" << it.row() << ", " << it.col() << ") " << it.value() << std::endl;  
+            }  
+        }
+    } 
+
+    DynamicSparseMatrixExt<T>* slice(int* array1, int size1, int* array2, int size2) { 
+        //Array indices must be sorted 
+        DynamicSparseMatrixExt *mat = new DynamicSparseMatrixExt<T>(size1, size2);
+        int size1Ind = 0; 
+
+        //Assume column major class - j is col index 
+        for (int j=0; j<size2; ++j) { 
+            size1Ind = 0; 
+            for (typename DynamicSparseMatrixExt<T>::InnerIterator it(*this, array2[j]); it; ++it) {
+                while (array1[size1Ind] < it.row() && size1Ind < size1) { 
+                    size1Ind++; 
+                    }
+
+                if(it.row() == array1[size1Ind]) { 
+                    mat->insert(size1Ind, j) = it.value();                    
+                    }
+
+                }    
+            }
+
+        return mat;
         }
   };
 

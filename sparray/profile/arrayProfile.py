1c07ab348c78d2d71c4ba8867feb9aae094ed9b2
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-12 20:45:39 +0200

    Profile sum, slice and nonzeros.

--- sparray/profile/arrayProfile.py
+++ sparray/profile/arrayProfile.py
@@ -5,19 +5,20 @@ from apgl.util import *
 from sparray.csr_array import csr_array 
 from sparray.dyn_array import dyn_array 
 from pysparse import spmatrix
+from apgl.util.PySparseUtils import PySparseUtils 
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 numpy.random.seed(21)
 
 
-class csr_arrayProfile():
+class arrayProfile():
     def __init__(self):
         #take some random indices 
         self.N = 5000 
-        M = 100 
+        self.M = 100 
         
-        self.rowInds = numpy.array(numpy.random.randint(0, M, self.N), dtype=numpy.int32)
-        self.colInds = numpy.array(numpy.random.randint(0, M, self.N), dtype=numpy.int32)
+        self.rowInds = numpy.array(numpy.random.randint(0, self.M, self.N), dtype=numpy.int)
+        self.colInds = numpy.array(numpy.random.randint(0, self.M, self.N), dtype=numpy.int)
         
         self.val = numpy.random.rand() 
         
@@ -51,8 +52,79 @@ class csr_arrayProfile():
                 A.put(self.val, self.rowInds, self.colInds)
         
         ProfileUtils.profile('runPut()', globals(), locals())
-    
-profiler = csr_arrayProfile()
-#profiler.profilePut()
-profiler.profilePut2()
-#profiler.profilePutPySparse()
+        
+    def profileSlicePys(self): 
+        A = spmatrix.ll_mat(self.N, self.N)  
+        A.put(self.val, self.rowInds, self.colInds)
+        
+        def runSlice():     
+            for i in range(10):  
+                sliceInds = numpy.array(numpy.random.randint(0, self.M, self.N), dtype=numpy.int32)
+                B = A[:, sliceInds]
+            
+        ProfileUtils.profile('runSlice()', globals(), locals())
+        
+    def profileSliceSpa(self): 
+        A = dyn_array((self.N, self.N))
+        A.put(self.val, self.rowInds, self.colInds)
+        
+        def runSlice():     
+            for i in range(10):  
+                sliceInds = numpy.array(numpy.random.randint(0, self.M, self.N), dtype=numpy.int)
+                B = A[:, sliceInds]
+            
+        ProfileUtils.profile('runSlice()', globals(), locals())
+        
+    def profileSumPys(self): 
+        A = spmatrix.ll_mat(self.N, self.N)  
+        A.put(self.val, self.rowInds, self.colInds)
+        
+        def runSum():     
+            for i in range(1000):  
+                 i = PySparseUtils.sum(A)
+            print(i)
+            
+        ProfileUtils.profile('runSum()', globals(), locals())
+        
+    def profileSumSpa(self): 
+        A = dyn_array((self.N, self.N))
+        A.put(self.val, self.rowInds, self.colInds)
+        
+        def runSum():     
+            for i in range(1000):  
+                 i = A.sum()
+            print(i)
+            
+        ProfileUtils.profile('runSum()', globals(), locals())
+        
+    def profileGetNonZerosPys(self): 
+        A = spmatrix.ll_mat(self.N, self.N)  
+        A.put(self.val, self.rowInds, self.colInds)
+        
+        def runNonZeros(): 
+            for i in range(1000):
+                (rows, cols) = PySparseUtils.nonzero(A)
+                nzVals = numpy.zeros(len(rows))
+                A.take(nzVals, rows, cols)
+            
+        ProfileUtils.profile('runNonZeros()', globals(), locals())
+
+    def profileGetNonZerosSpa(self): 
+        A = dyn_array((self.N, self.N)) 
+        A.put(self.val, self.rowInds, self.colInds)
+        
+        def runNonZeros(): 
+            for i in range(1000):
+                rows, cols = A.nonzero()
+                vals = A[rows, cols]
+            print(numpy.sum(vals))
+            
+        ProfileUtils.profile('runNonZeros()', globals(), locals())
+
+profiler = arrayProfile()
+#profiler.profileSlicePys()
+#profiler.profileSliceSpa()
+#profiler.profileSumPys()
+#profiler.profileSumSpa()
+#profiler.profileGetNonZerosPys()
+profiler.profileGetNonZerosSpa()

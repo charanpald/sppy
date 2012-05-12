1f38e7227f46163f8f9bcbbfadd22c6185abd9c8
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-12 20:46:01 +0200

    Testing of nonZeroInds + more.

--- sparray/test/dyn_arrayTest.py
+++ sparray/test/dyn_arrayTest.py
@@ -9,13 +9,8 @@ from sparray.dyn_array import dyn_array
 class dyn_arrayTest(unittest.TestCase):
     def setUp(self):
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-        
         self.A = dyn_array((5, 5))
-        #self.A[0, 1] = 1 
-        #self.A[1, 1] = 2 
-        #self.A[2, 1] = 5 
-        
-        
+
         nrow = 5 
         ncol = 7
         self.B = dyn_array((nrow, ncol))
@@ -25,10 +20,17 @@ class dyn_arrayTest(unittest.TestCase):
         self.B[0, 6] = -1.23
         self.B[4, 4] = 12.2        
         
+        nrow = 100 
+        ncol = 100
+        self.C = dyn_array((nrow, ncol))
+        self.C[0, 1] = 1
+        self.C[10, 3] = 5.2
+        self.C[30, 34] = -0.2
+        self.C[0, 62] = -1.23
+        self.C[4, 41] = 12.2      
+        
     def testInit(self): 
         A = dyn_array((5, 7))
-        #A[0, 1] = 1 
-        
         self.assertEquals(A.shape, (5, 7))
         
         A = dyn_array((1, 1))
@@ -40,14 +42,26 @@ class dyn_arrayTest(unittest.TestCase):
         A = dyn_array((0, 0))
         self.assertEquals(A.shape, (0, 0))
         
-        #Test dtypes 
+        #Test bad input params 
+        self.assertRaises(IndexError, dyn_array, (0,))
+        self.assertRaises(TypeError, dyn_array, 0)
+        
+        #TODO: Test other dtypes 
         A = dyn_array((5, 5))
-        A[0, 0] = 1.5 
+        self.assertEquals(A.dtype, numpy.float)
         
         
     def testNDim(self): 
         A = dyn_array((5, 7))
         self.assertEquals(A.ndim, 2)
+        
+        A = dyn_array((0, 0))
+        self.assertEquals(A.ndim, 2)
+    
+    def testSize(self): 
+        self.assertEquals(self.A.size, 25)
+        self.assertEquals(self.B.size, 35)
+        self.assertEquals(self.C.size, 10000)
         
     def testGetnnz(self): 
        A = dyn_array((5, 7))
@@ -69,6 +83,16 @@ class dyn_arrayTest(unittest.TestCase):
        B = dyn_array((5, 7))
        B[(numpy.array([1, 2, 3]), numpy.array([4, 5, 6]))] = 1
        self.assertEquals(B.getnnz(), 3)
+       
+       for i in range(5): 
+           for j in range(7): 
+               B[i, j] = 1
+               
+       self.assertEquals(B.getnnz(), 35)
+       
+       self.assertEquals(self.A.getnnz(), 0)
+       self.assertEquals(self.B.getnnz(), 5)
+       self.assertEquals(self.C.getnnz(), 5)
     
     def testSetItem(self):
         nrow = 5 
@@ -79,7 +103,7 @@ class dyn_arrayTest(unittest.TestCase):
         A[3, 3] = -0.2
         
         self.assertEquals(A[0, 1], 1)
-        self.assertAlmostEquals(A[1, 3], 5.2, 5)
+        self.assertAlmostEquals(A[1, 3], 5.2)
         self.assertAlmostEquals(A[3, 3], -0.2)
         
         for i in range(nrow): 
@@ -87,17 +111,11 @@ class dyn_arrayTest(unittest.TestCase):
                 if (i, j) != (0, 1) and (i, j) != (1, 3) and (i, j) != (3, 3): 
                     self.assertEquals(A[i, j], 0)
         
-        try: 
-            A[20, 0] = 1
-            self.fail()
-        except ValueError: 
-            pass 
-        
-        try: 
-            A[0, 20] = 1
-            self.fail()
-        except ValueError: 
-            pass 
+        self.assertRaises(ValueError, A.__setitem__, (20, 1), 1)  
+        self.assertRaises(TypeError, A.__setitem__, (1, 1), "a")   
+        self.assertRaises(ValueError, A.__setitem__, (1, 100), 1)   
+        self.assertRaises(ValueError, A.__setitem__, (-1, 1), 1)   
+        self.assertRaises(ValueError, A.__setitem__, (0, -1), 1) 
         
         result = A[(numpy.array([0, 1, 3]), numpy.array([1, 3, 3]))] 
         self.assertEquals(result[0], 1)
@@ -107,7 +125,7 @@ class dyn_arrayTest(unittest.TestCase):
         #Replace value of A 
         A[0, 1] = 2
         self.assertEquals(A[0, 1], 2)
-        self.assertAlmostEquals(A[1, 3], 5.2, 5)
+        self.assertAlmostEquals(A[1, 3], 5.2)
         self.assertAlmostEquals(A[3, 3], -0.2)
         
         for i in range(nrow): 
@@ -124,7 +142,15 @@ class dyn_arrayTest(unittest.TestCase):
         A[1, 3] = 5.2
         A[3, 3] = -0.2
         
-        #print(A)
+        outputStr = "dyn_array shape:(5, 7) non-zeros:3\n" 
+        outputStr += "(0, 1) 1.0\n"
+        outputStr += "(1, 3) 5.2\n"
+        outputStr += "(3, 3) -0.2\n"
+        self.assertEquals(str(A), outputStr) 
+        
+        B = dyn_array((5, 5))
+        outputStr = "dyn_array shape:(5, 5) non-zeros:0\n" 
+        self.assertEquals(str(B), outputStr) 
 
     def testSum(self): 
         nrow = 5 
@@ -144,6 +170,10 @@ class dyn_arrayTest(unittest.TestCase):
         
         A[0, 0] = 0 
         self.assertEquals(A.sum(), 4.8)
+        
+        self.assertEquals(self.A.sum(), 0.0)
+        self.assertEquals(self.B.sum(), 16.97)
+        self.assertEquals(self.C.sum(), 16.97)
 
     def testGet(self): 
         self.assertEquals(self.B[0, 1], 1)
@@ -151,14 +181,21 @@ class dyn_arrayTest(unittest.TestCase):
         self.assertEquals(self.B[3, 3], -0.2)
         self.assertEquals(self.B.getnnz(), 5)
         
-        #self.B
+        self.assertRaises(ValueError, self.B.__getitem__, (20, 1))  
+        self.assertRaises(ValueError, self.B.__getitem__, (1, 20))  
+        self.assertRaises(ValueError, self.B.__getitem__, (-1, 1))  
+        self.assertRaises(ValueError, self.B.__getitem__, (1, -1))  
+        self.assertRaises(TypeError, self.B.__getitem__, (1))
+        self.assertRaises(ValueError, self.B.__getitem__, "a")
+        
+        #Test array slicing 
         C = self.B[numpy.array([0, 1, 3]), numpy.array([1, 3, 3])]
         self.assertEquals(C.shape[0], 3)
         self.assertEquals(C[0], 1)
         self.assertEquals(C[1], 5.2)
         self.assertEquals(C[2], -0.2)
         
-        """
+        
         C = self.B[numpy.array([0, 1, 3]), :]
         self.assertEquals(C.shape, (3, 7))
         self.assertEquals(C.getnnz(), 4)
@@ -166,21 +203,40 @@ class dyn_arrayTest(unittest.TestCase):
         self.assertEquals(C[1, 3], 5.2)
         self.assertEquals(C[2, 3], -0.2)
         self.assertEquals(C[0, 6], -1.23)
-        """
+        
         
     def testArraySlice(self): 
-        rowInds = numpy.array([0, 1], numpy.int32)
-        colInds = numpy.array([1, 3, 6], numpy.int32)
+        rowInds = numpy.array([0, 1], numpy.int)
+        colInds = numpy.array([1, 3, 6], numpy.int)
         A = self.B.arraySlice(rowInds, colInds)
         
         for i in range(A.shape[0]): 
             for j in range(A.shape[1]): 
                 self.assertEquals(A[i, j], self.B[rowInds[i], colInds[j]])
                 
-
+    def testNonZeroInds(self): 
+        
+        (rowInds, colInds) = self.B.nonzero()
+        
+        for i in range(rowInds.shape[0]): 
+            self.assertNotEqual(self.B[rowInds[i], colInds[i]], 0)
+        
+        (rowInds, colInds) = self.C.nonzero()
+        
+        for i in range(rowInds.shape[0]): 
+            self.assertNotEqual(self.C[rowInds[i], colInds[i]], 0)        
+        
+        #Try an array with no non zeros 
+        nrow = 5 
+        ncol = 7
+        A = dyn_array((nrow, ncol))
+        (rowInds, colInds) = A.nonzero()
+        
+        self.assertEquals(rowInds.shape[0], 0)
+        self.assertEquals(colInds.shape[0], 0)
+        
         
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
     
     

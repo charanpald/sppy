7e86fe44a7affb9e329178b345a3edbff43a4d0c
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-20 20:53:56 +0200

    factor out include dirs.

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools.extension import Extension
 from setuptools import setup
 from Cython.Distutils import build_ext
 
+include  = ["/usr/include/eigen3/"]
+
 setup(name = 'sparray',
       version = 0.1,
       author = 'Charanpal Dhanjal',
       author_email = 'charanpal@gmail.com',
-      ext_modules=[Extension("sparray.csr_array", ["sparray/csr_array.pyx"], language="c++", include_dirs=["/usr/include/eigen3/"]), 
-                   Extension("sparray.dyn_array", ["sparray/dyn_array.pyx"], language="c++", include_dirs=["/usr/include/eigen3/"])],  
+      ext_modules=[Extension("sparray.csr_array", ["sparray/csr_array.pyx"], language="c++", include_dirs=include), 
+                   Extension("sparray.dyn_array", ["sparray/dyn_array.pyx"], language="c++", include_dirs=include)],  
       cmdclass={'build_ext': build_ext})

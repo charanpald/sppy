3a129e21970b0a67a77985b4bbf7cd658a848e14
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-26 19:20:38 +0200

    Support for compiling without cython.

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
-#from distutils.core import setup
-from setuptools.extension import Extension
-from setuptools import setup
-from Cython.Distutils import build_ext
+from distutils.core import setup
+from distutils.extension import Extension
 
+#Set this to the path of the Eigen library on your system 
 include  = ["/usr/include/eigen3/"]
 
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+cmdclass = { }
+
+if use_cython:
+    ext_modules=[Extension("sparray.csr_array", ["sparray/csr_array.pyx"], language="c++", include_dirs=include),
+                 Extension("sparray.dyn_array", ["sparray/dyn_array.pyx"], language="c++", include_dirs=include)]  
+    cmdclass.update({ 'build_ext': build_ext })
+else:
+    ext_modules = [Extension("sparray.csr_array", [ "sparray/csr_array.cpp" ], include_dirs=include),
+                    Extension("sparray.dyn_array", [ "sparray/dyn_array.cpp" ], include_dirs=include)] 
+
 setup(name = 'sparray',
-      version = 0.1,
+      version = "0.1",
       author = 'Charanpal Dhanjal',
       author_email = 'charanpal@gmail.com',
-      ext_modules=[Extension("sparray.csr_array", ["sparray/csr_array.pyx"], language="c++", include_dirs=include), 
-                   Extension("sparray.dyn_array", ["sparray/dyn_array.pyx"], language="c++", include_dirs=include)],  
-      cmdclass={'build_ext': build_ext})
+      packages = ['sparray', 'sparray.test'],
+      ext_modules=ext_modules, 
+      cmdclass=cmdclass)

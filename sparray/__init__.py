79fc903c7c6186692252a71defadf08234a5caaa
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-26 19:20:54 +0200

    Testing function and import arrays.

---
+++ sparray/__init__.py
@@ -0,0 +1,27 @@
+from sparray.csr_array import csr_array
+from sparray.dyn_array import dyn_array
+from sparray.version import __version__
+
+def test():
+    """
+    A function which uses the unittest library to find all tests within sparray (those files
+    matching "*Test.py"), and run those tests. 
+    """
+    try:
+        import traceback
+        import sys
+        import os
+        import unittest
+        import logging
+
+        logging.disable(logging.WARNING)
+        print(os.path.dirname(__file__))
+        sourceDir = os.path.dirname(__file__) + os.sep
+        print("Running tests from " + sourceDir)
+
+        overallTestSuite = unittest.TestSuite()
+        overallTestSuite.addTest(unittest.defaultTestLoader.discover(sourceDir, pattern='*Test.py', top_level_dir=sourceDir))
+        unittest.TextTestRunner(verbosity=1).run(overallTestSuite)
+    except ImportError as error:
+        traceback.print_exc(file=sys.stdout)
+

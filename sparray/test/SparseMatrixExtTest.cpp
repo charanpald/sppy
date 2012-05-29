b68858abdafd40527ae7f1a00e0b03bf466dd654
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-29 21:39:30 +0200

    Test copy constructor.

--- sparray/test/SparseMatrixExtTest.cpp
+++ sparray/test/SparseMatrixExtTest.cpp
@@ -7,28 +7,27 @@
 
 using Eigen::SparseMatrix;
 
-int main()
-{
+void testScalarMultiply() { 
     DynamicSparseMatrixExt<double> m(3,3);
     m.insert(0,0) = 3;
     m.insert(1,0) = 2.5;
     m.insert(0,1) = -1;
     m.insert(2,0) = -1;    
     m.insert(2,2) = 5.12;
-
-    DynamicSparseMatrixExt<double> m2(3,3);
-    m2.insert(0,0) = 3;
-    m2.insert(1,0) = 2.5;
-    m2.insert(0,1) = -1;
-    m2.insert(2,0) = -1;    
-    m2.insert(2,2) = 5.12;
     
-    //m.printValues();
-    //m2.printValues();
+    DynamicSparseMatrixExt<double> m2(m);
+    m.scalarMultiply(2.0);
 
-    DynamicSparseMatrixExt<double> m3; 
-    m3 = m2;
+    m.printValues();
     m2.printValues();
-    m3.printValues();
+
+    //DynamicSparseMatrix<double> m2(3,3); 
+    //DynamicSparseMatrix<double> m3(m2);
+}
+
+
+int main()
+{
+    testScalarMultiply();
 
 }

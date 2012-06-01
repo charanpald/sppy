1ebd1546bc04e943261ddba21626c4714f0f274f
Charanpal Dhanjal <charanpal@gmail.com>
2012-06-01 21:24:04 +0100

    Test add

--- sparray/test/SparseMatrixExtTest.cpp
+++ sparray/test/SparseMatrixExtTest.cpp
@@ -7,27 +7,50 @@
 
 using Eigen::SparseMatrix;
 
+
 void testScalarMultiply() { 
-    DynamicSparseMatrixExt<double> m(3,3);
+    DynamicSparseMatrixExt<double, Eigen::ColMajor> m(3,3);
     m.insert(0,0) = 3;
     m.insert(1,0) = 2.5;
     m.insert(0,1) = -1;
     m.insert(2,0) = -1;    
     m.insert(2,2) = 5.12;
     
-    DynamicSparseMatrixExt<double> m2(m);
+    DynamicSparseMatrixExt<double, Eigen::ColMajor> m2(m);
     m.scalarMultiply(2.0);
 
     m.printValues();
     m2.printValues();
+}
 
-    //DynamicSparseMatrix<double> m2(3,3); 
-    //DynamicSparseMatrix<double> m3(m2);
+void testMatrixAdd() { 
+    DynamicSparseMatrixExt<double, Eigen::ColMajor> m(3,3);
+    m.insert(0,0) = 3;
+    m.insert(1,0) = 2.5;
+    m.insert(0,1) = -1;
+    m.insert(2,0) = -1;    
+    m.insert(2,2) = 5.12;
+
+    DynamicSparseMatrixExt<double, Eigen::ColMajor> m2(3, 3);
+    m2.insert(0, 0) = 2;
+    
+    DynamicSparseMatrixExt<double> m3(3, 3);
+
+    m.printValues();
+    m3.printValues();
+
+    m = m2; 
+    m.printValues();
+
+    m3 = m + m2; 
+    
 }
 
 
 int main()
 {
-    testScalarMultiply();
+
+
+    testMatrixAdd();
 
 }

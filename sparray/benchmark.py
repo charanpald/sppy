6b47ab503f6c8463a5bc8b36e4dd9d6a574bf269
Charanpal Dhanjal <charanpal@gmail.com>
2012-08-08 19:15:10 +0100

    Plot benchmark results.

--- sparray/benchmark.py
+++ sparray/benchmark.py
@@ -6,6 +6,7 @@ import time
 import numpy 
 import logging 
 from sparray import csarray 
+import matplotlib.pyplot as plt 
 from scipy.sparse import csc_matrix, csr_matrix
 
 
@@ -13,10 +14,24 @@ from scipy.sparse import csc_matrix, csr_matrix
 #Get a selection of elements 
 #Col and row slice 
 
-def setGetBenchmark(ns): 
-    numVals = 5000 
-    
+def getRandomElements(numVals, shape, seed=21): 
+    numpy.random.seed(seed) 
     vals = numpy.random.rand(numVals)
+    
+    rowInds = numpy.random.randint(0, shape[0], numVals)        
+    colInds = numpy.random.randint(0, shape[1], numVals)
+    
+    return rowInds, colInds, vals
+
+def setRandomElements(numVals, A, seed=21):
+    rowInds, colInds, vals = getRandomElements(numVals, A.shape, seed)
+    
+    for j in range(numVals): 
+        A[rowInds[j], colInds[j]] = vals[j]
+
+def setGetBenchmark(ns): 
+    numVals = 1000 
+    
     numMethods = 3
     setTimeArray = numpy.zeros((numMethods, len(ns)))
     getTimeArray = numpy.zeros((numMethods, len(ns)))
@@ -25,9 +40,8 @@ def setGetBenchmark(ns):
     for s in range(len(ns)):
         n = ns[s]
         logging.debug("n="+str(n))
-        numpy.random.seed(21)
-        rowInds = numpy.random.randint(0, n, numVals)        
-        colInds = numpy.random.randint(0, n, numVals)
+        
+        rowInds, colInds, vals = getRandomElements(numVals, (n, n))
         
         logging.debug("Benchmarking set from csc_matrix")
         startTime = time.clock()
@@ -64,7 +78,7 @@ def setGetBenchmark(ns):
         logging.debug("Benchmarking set from csarray")
         startTime = time.clock()
         for i in range(repetitions):
-            A1 = csarray((n, n))            
+            A1 = csarray((n, n))     
             for j in range(numVals): 
                 A1[rowInds[j], colInds[j]] = vals[j]
         setTimeArray[2, s] =  time.clock() - startTime
@@ -81,7 +95,6 @@ def setGetBenchmark(ns):
 
 def addMultBenchmark(ns): 
     numVals = 1000 
-    vals = numpy.random.rand(numVals)
     numMethods = 3
     addTimeArray = numpy.zeros((numMethods, len(ns)))
     multTimeArray = numpy.zeros((numMethods, len(ns)))
@@ -90,19 +103,12 @@ def addMultBenchmark(ns):
     for s in range(len(ns)):
         n = ns[s]
         logging.debug("n="+str(n))
-        numpy.random.seed(21)
-        rowInds1 = numpy.random.randint(0, n, numVals)        
-        colInds1 = numpy.random.randint(0, n, numVals)
-        rowInds2 = numpy.random.randint(0, n, numVals)        
-        colInds2 = numpy.random.randint(0, n, numVals)
              
-        print("Benchmarking csc_matrix")
+        print("Benchmarking +/* of csc_matrix")
         A1 = csc_matrix((n, n))       
         A2 = csc_matrix((n, n)) 
-        
-        for j in range(numVals): 
-            A1[rowInds1[j], colInds1[j]] = vals[j]
-            A2[rowInds2[j], colInds2[j]] = vals[j]
+        setRandomElements(numVals, A1, 21)
+        setRandomElements(numVals, A2, 22)
         
         startTime = time.clock()
         for i in range(repetitions): 
@@ -114,13 +120,11 @@ def addMultBenchmark(ns):
             B = A1 * A2
         multTimeArray[0, s] =  time.clock() - startTime 
         
-        print("Benchmarking csr_matrix")
+        print("Benchmarking  +/* of csr_matrix")
         A1 = csr_matrix((n, n))       
         A2 = csr_matrix((n, n)) 
-        
-        for j in range(numVals): 
-            A1[rowInds1[j], colInds1[j]] = vals[j]
-            A2[rowInds2[j], colInds2[j]] = vals[j]
+        setRandomElements(numVals, A1, 21)
+        setRandomElements(numVals, A2, 22)
         
         startTime = time.clock()
         for i in range(repetitions): 
@@ -132,14 +136,11 @@ def addMultBenchmark(ns):
             B = A1 * A2
         multTimeArray[1, s] =  time.clock() - startTime 
         
-        print("Benchmarking csarray")
+        print("Benchmarking  +/* of csarray")
         A1 = csarray((n, n))       
         A2 = csarray((n, n)) 
-        
-        for j in range(numVals): 
-            A1[rowInds1[j], colInds1[j]] = vals[j]
-            A2[rowInds2[j], colInds2[j]] = vals[j]
-        
+        setRandomElements(numVals, A1, 21)
+        setRandomElements(numVals, A2, 22)
         A1.compress()
         A2.compress()        
         
@@ -157,46 +158,35 @@ def addMultBenchmark(ns):
     
 def meanSumBenchmark(ns): 
     numVals = 1000 
-    vals = numpy.random.rand(numVals)
     numMethods = 3
     sumTimeArray = numpy.zeros((numMethods, len(ns)))
-    repetitions = 1000 
+    repetitions = 100 
     
     for s in range(len(ns)):
         n = ns[s]
         logging.debug("n="+str(n))
-        numpy.random.seed(21)
-        rowInds = numpy.random.randint(0, n, numVals)        
-        colInds = numpy.random.randint(0, n, numVals)
-             
-        print("Benchmarking csc_matrix")
+
+        print("Benchmarking sum of csc_matrix")
         A = csc_matrix((n, n))       
-        
-        for j in range(numVals): 
-            A[rowInds[j], colInds[j]] = vals[j]
+        setRandomElements(numVals, A, 21)
 
         startTime = time.clock()
         for i in range(repetitions): 
             b = A.sum()
         sumTimeArray[0, s] =  time.clock() - startTime 
         
-        print("Benchmarking csr_matrix")
+        print("Benchmarking sum of csr_matrix")
         A = csr_matrix((n, n))       
-        
-        for j in range(numVals): 
-            A[rowInds[j], colInds[j]] = vals[j]
+        setRandomElements(numVals, A, 21)
 
         startTime = time.clock()
         for i in range(repetitions): 
             b = A.sum()
         sumTimeArray[1, s] =  time.clock() - startTime 
         
-        print("Benchmarking csarray")
+        print("Benchmarking sum of csarray")
         A = csarray((n, n))       
-        
-        for j in range(numVals): 
-            A[rowInds[j], colInds[j]] = vals[j]
-            
+        setRandomElements(numVals, A, 21)
         A.compress()
 
         startTime = time.clock()
@@ -210,13 +200,58 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.set_printoptions(suppress=True)
 
 ns = [100, 500, 1000, 5000, 10000, 50000, 100000]
+#ns = [100, 500, 1000, 5000]
+methodNames = ["csc_matrix", "csr_matrix", "csarray"]
 
 setTimeArray, getTimeArray = setGetBenchmark(ns)
 addTimeArray, multTimeArray = addMultBenchmark(ns)
 sumTimeArray = meanSumBenchmark(ns)
+
+plt.figure(0)
+for i in range(len(methodNames)): 
+    plt.plot(ns, setTimeArray[i, :], label=methodNames[i])
+plt.title("set")
+plt.xlabel("x")
+plt.ylabel("time")
+plt.legend()
+
+plt.figure(1)
+for i in range(len(methodNames)): 
+    plt.plot(ns, getTimeArray[i, :], label=methodNames[i])
+plt.title("get")
+plt.xlabel("x")
+plt.ylabel("time")
+plt.legend()
+
+plt.figure(2)
+for i in range(len(methodNames)): 
+    plt.plot(ns, addTimeArray[i, :], label=methodNames[i])
+plt.title("add")
+plt.xlabel("x")
+plt.ylabel("time")
+plt.legend()
+
+plt.figure(3)
+for i in range(len(methodNames)): 
+    plt.plot(ns, multTimeArray[i, :], label=methodNames[i])
+plt.title("mult")
+plt.xlabel("x")
+plt.ylabel("time")
+plt.legend()
+
+plt.figure(4)
+for i in range(len(methodNames)): 
+    plt.plot(ns, sumTimeArray[i, :], label= methodNames[i])
+plt.title("sum")
+plt.xlabel("x")
+plt.ylabel("time")
+plt.legend()
 
 print(setTimeArray)
 print(getTimeArray)
 print(addTimeArray)
 print(multTimeArray)
 print(sumTimeArray)
+
+plt.show()
+

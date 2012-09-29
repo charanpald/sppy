76c4b5a19b5d19d9a764ff59f215d6e9622fae85
Charanpal Dhanjal <charanpal@gmail.com>
2012-09-29 10:40:50 +0100

    Testing for vectors.

--- sppy/test/utilTest.py
+++ sppy/test/utilTest.py
@@ -3,8 +3,6 @@ import sys
 import unittest
 import numpy
 import numpy.testing as nptst 
-import math 
-
 import sppy 
 
 class utilTest(unittest.TestCase):
@@ -37,22 +35,33 @@ class utilTest(unittest.TestCase):
         self.assertEquals(X.dtype, numpy.int)
         
     def testRand(self): 
-        X = sppy.rand(10, 10, 0.1)
+        X = sppy.rand((10, 10), 0.1)
 
         self.assertEquals(X.shape, (10, 10))
         self.assertAlmostEqual(X.getnnz()/float(100), 0.1, 1)     
         
+        X = sppy.rand(100, 0.1)
+
+        self.assertEquals(X.shape, (100, ))
+        self.assertAlmostEqual(X.getnnz()/float(100), 0.1, 1)  
+        
     def testZeros(self): 
         X = sppy.zeros((5, 5))
-
         self.assertEquals(X.shape, (5, 5))
         nptst.assert_array_equal(X.toarray(), numpy.zeros((5,5)))
         
+        x = sppy.zeros(5)
+        self.assertEquals(x.shape, (5, ))
+        nptst.assert_array_equal(x.toarray(), numpy.zeros((5,)))
+        
     def testOnes(self): 
         X = sppy.ones((5, 5))
-
         self.assertEquals(X.shape, (5, 5))
-        nptst.assert_array_equal(X.toarray(), numpy.ones((5,5)))   
+        nptst.assert_array_equal(X.toarray(), numpy.ones((5,5)))  
+        
+        x = sppy.ones((5, ))
+        self.assertEquals(x.shape, (5, ))
+        nptst.assert_array_equal(x.toarray(), numpy.ones((5,))) 
         
 
 if __name__ == "__main__":

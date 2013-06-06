bf83a8d3919e7a89fdac99e4cedb8400a19a46cc
Charanpal Dhanjal <charanpal@gmail.com>
2013-06-06 21:40:18 +0200

    Check for scipy.linalg

--- sppy/linalg/test/GeneralLinearOperatorTest.py
+++ sppy/linalg/test/GeneralLinearOperatorTest.py
@@ -4,7 +4,6 @@ import unittest
 import numpy
 import numpy.testing as nptst 
 import math 
-import scipy.sparse.linalg
 
 from sppy import csarray, rand 
 from sppy.linalg import GeneralLinearOperator
@@ -21,26 +20,29 @@ class GeneralLinearOperatorTest(unittest.TestCase):
         
         
     def testAsLinearOperator(self): 
-        #Check it works for the eigenvalue solver 
-        L = GeneralLinearOperator.asLinearOperator(self.A)
-        k = 10 
-        s, V = scipy.sparse.linalg.eigsh(L, k, which="LA")
-        
-        inds = numpy.flipud(numpy.argsort(s))
-        s = s[inds]  
-        V = V[:, inds]
-        
-        B = self.A.toarray()
-        s2, V2 = numpy.linalg.eigh(B)
-        
-        inds = numpy.flipud(numpy.argsort(s2))[0:k]
-        s2 = s2[inds]
-        V2 = V2[:, inds]
-        
-        nptst.assert_array_almost_equal(s, s2[0:s.shape[0]])
-        
-        nptst.assert_array_almost_equal((V*s).dot(V.T), (V2*s2).dot(V2.T)) 
-        
+        try: 
+            import scipy.sparse.linalg
+            #Check it works for the eigenvalue solver 
+            L = GeneralLinearOperator.asLinearOperator(self.A)
+            k = 10 
+            s, V = scipy.sparse.linalg.eigsh(L, k, which="LA")
+            
+            inds = numpy.flipud(numpy.argsort(s))
+            s = s[inds]  
+            V = V[:, inds]
+            
+            B = self.A.toarray()
+            s2, V2 = numpy.linalg.eigh(B)
+            
+            inds = numpy.flipud(numpy.argsort(s2))[0:k]
+            s2 = s2[inds]
+            V2 = V2[:, inds]
+            
+            nptst.assert_array_almost_equal(s, s2[0:s.shape[0]])
+            
+            nptst.assert_array_almost_equal((V*s).dot(V.T), (V2*s2).dot(V2.T)) 
+        except:
+            print("Couldn't test testAsLinearOperator") 
         
 if __name__ == "__main__":
     unittest.main()

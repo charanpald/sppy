cd9668539ff11860d73c0c0a4067e5c339e86291
Charanpal Dhanjal <charanpal@gmail.com>
2014-02-05 21:33:39 +0100

    mmread function

--- sppy/io/matrix_market.py
+++ sppy/io/matrix_market.py
@@ -1,4 +1,7 @@
 
+import numpy 
+import string
+from sppy import csarray
 """
 Some functions to read and write matrix market files. 
 """
@@ -40,7 +43,50 @@ def mmwrite(filename, A, comment='', field=None, precision=None):
     
     fileObj.close()
         
+def mmread(filename, storagetype="col"): 
+    """
+    Read from a matrix market file. Note that we do not allow comments (%) in the 
+    body of the elements, only in the header lines. 
+    """
+    fileObj = open(filename, "r")
     
+    line = fileObj.readline()
+    line.strip().strip("#")
     
+    vals = line.split()
     
+    if vals[3] == "integer": 
+        dtype = numpy.int 
+    elif vals[3] == "real": 
+        dtype = numpy.float 
+    else:
+        raise ValueError("Invalid data type: " + vals[3])
     
+    line = fileObj.readline()
+    while string.find(line, "%") == 0: 
+        line = fileObj.readline()    
+    
+    vals = line.split()
+    m = int(vals[0])
+    n = int(vals[1])
+    nnz = int(vals[2])
+    
+    rowInds = numpy.zeros(nnz, numpy.int32)
+    colInds = numpy.zeros(nnz, numpy.int32)
+    values = numpy.zeros(nnz, dtype)
+    
+    #This reads the rest of the file reasonably quickly 
+    Z = numpy.fromfile(fileObj, sep=" ")
+    Z = Z.reshape((nnz, 3))
+
+    rowInds = numpy.array(Z[:, 0], numpy.int32)-1 
+    colInds = numpy.array(Z[:, 1], numpy.int32)-1 
+    values = numpy.ascontiguousarray(Z[:, 2], dtype=dtype)
+
+    fileObj.close()
+
+    A = csarray((m, n), dtype=dtype, storagetype=storagetype)
+    A.reserve(nnz)
+    A.put(values, rowInds, colInds, init=True)
+    
+    return A     

b593d1f4f0e6d33d8826d7e152a8ff7ad7ffca3c
Charanpal <charanpal@gmail.com>
2015-02-26 21:47:24 +0000

    Timing of matrix multiplication

---
+++ sppy/test/SparseMatrixExtTime.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Sparse>
+#include <time.h> 
+
+using namespace Eigen;
+
+std::vector<long> get_vector_of_rn_longs(long length, long min, long max, int seed);
+std::vector<double> get_vector_of_rn_doubles(int length, double min, double max);
+
+int main()
+{
+
+  long N_COLS = 10000;
+  long N_ROWS = 10000;
+  long N_VALUES = N_COLS*N_ROWS*0.01;
+
+  std::cout << N_VALUES << std::endl;
+
+  SparseMatrix<double, RowMajor> big_A(N_ROWS, N_COLS);
+  std::vector<long> cols_a = get_vector_of_rn_longs(N_VALUES, 0, N_COLS, 342);
+  std::vector<long> rows_a = get_vector_of_rn_longs(N_VALUES, 0, N_ROWS, 43);
+  std::vector<double> values_a = get_vector_of_rn_doubles(N_VALUES, 0, 1);
+
+  for (int i = 0; i < N_VALUES; i++) {
+    //std::cout << rows_a[i] << " " << cols_a[i] << " " << std::endl;
+    big_A.coeffRef(rows_a[i], cols_a[i]) = values_a[i];
+    } 
+std::cout << big_A.nonZeros() << "  " << big_A.rows() << std::endl;
+    big_A.makeCompressed();
+
+  SparseMatrix<double, ColMajor> big_B(N_ROWS, N_COLS);
+  //std::vector<long> cols_b = gen_random_sample(0, N_COLS, N_VALUES);
+  //std::vector<long> rows_b = gen_random_sample(0, N_COLS, N_VALUES);
+  //std::vector<double> values_b = get_vector_of_rn_doubles(N_VALUES, 0, 1);
+
+  //for (int i = 0; i < N_VALUES; i++)
+  //  big_B.coeffRef(cols_b[i], cols_b[i]) += values_b[i];
+    big_B = big_A.transpose();
+    big_B.makeCompressed();
+
+  SparseMatrix<double> big_AB(N_ROWS, N_COLS);
+
+  clock_t begin = clock();
+
+  big_AB = (big_A * big_B); //.pruned();
+
+  clock_t end = clock();
+  double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+  std::cout << "Time taken : " << elapsed_secs << std::endl;
+
+}
+
+std::vector<long> get_vector_of_rn_longs(long length, long min, long max, int seed)
+{
+    std::default_random_engine engine(seed);
+    std::uniform_int_distribution<long> distribution(min, max-1);
+    std::vector<long> my_vector(length);
+
+    auto generator = std::bind(distribution, engine);
+    std::generate_n(my_vector.begin(), length, generator); 
+
+  return my_vector;
+}
+
+std::vector<double> get_vector_of_rn_doubles(int length, double min, double max)
+{
+
+    std::uniform_real_distribution<double> unif(min, max);
+    std::default_random_engine re;
+    std::vector<double> my_vector(length);
+
+    for (int i=0; i < length; i++) {
+        my_vector[i] = unif(re);
+        }
+  return my_vector;
+}
